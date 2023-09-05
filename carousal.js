59673c1baa8373a773117a00ffd56619f4555cc5
ovaid <ovaidmushtaq.ovi@gmail.com>
2023-09-05 18:03:31 +0530

    newOne

---
+++ carousal.js
@@ -0,0 +1,13 @@
+// const slider = document.querySelector('.slider');
+//       const slides = document.querySelectorAll('.slide');
+//       let slideWidth = slides[0].offsetWidth;
+
+//       let currentIndex = 0
+
+//       function moveSlider(){
+        
+//         currentIndex = (currentIndex + 1) % slides.length;
+       
+//         slider.style.transform = 'translateX(-${curentIndex * slideWidth}px)';
+//       }
+//       setInterval(moveSlider, 2000);
